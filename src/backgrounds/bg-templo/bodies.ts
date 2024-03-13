a14ae0bd0cf36112ac36a4681dd403e08bfe86d1
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-03-13 16:10:27 -0300

    mejore la adaptabilidad de casi todos los backgrounds

--- src/backgrounds/bg-templo/bodies.ts
+++ src/backgrounds/bg-templo/bodies.ts
@@ -4,29 +4,25 @@ import type { ICanvasBounds } from "@utils/matterjs/canvas";
 import { quinacridoneMagentaColor, brightPinkCrayolaColor } from "@utils/matterjs/colors";
 import Matter, { Body } from "matter-js";
 
-export const createBodies = (bounds: ICanvasBounds): Matter.Body[] => {
-	const DELTA = 0.25 * bounds.height;
+interface ITemple {
+	x: number;
+	bounds: ICanvasBounds;
+}
 
-	const sun = createCircle({
-		x: bounds.width,
-		y: DELTA,
-		radio: 0.25 * bounds.width,
-		fillColor: brightPinkCrayolaColor,
-	});
+const createTemple = ({ x, bounds }: ITemple): Matter.Body => {
+	const HEIGHT_FLOOR = 0.3 * Math.min(bounds.width, bounds.height);
 
-	const HEIGHT_FLOOR = 0.3 * bounds.width;
-
-	const temple = Body.create({
+	return Body.create({
 		parts: [
 			createRectangle({
-				x: 0,
+				x,
 				y: bounds.height,
 				width: bounds.width,
 				height: HEIGHT_FLOOR,
 				fillColor: quinacridoneMagentaColor,
 			}),
 			createTrapezoid({
-				x: 0,
+				x,
 				y: bounds.height - HEIGHT_FLOOR,
 				width: 1.5 * bounds.width,
 				height: 0.25 * bounds.width,
@@ -34,14 +30,14 @@ export const createBodies = (bounds: ICanvasBounds): Matter.Body[] => {
 				fillColor: brightPinkCrayolaColor,
 			}),
 			createRectangle({
-				x: 0,
+				x,
 				y: bounds.height - 2 * HEIGHT_FLOOR,
 				width: 0.5 * bounds.width,
 				height: HEIGHT_FLOOR,
 				fillColor: quinacridoneMagentaColor,
 			}),
 			createTrapezoid({
-				x: 0,
+				x,
 				y: bounds.height - 3 * HEIGHT_FLOOR,
 				width: 1 * bounds.width,
 				height: 0.25 * bounds.width,
@@ -49,14 +45,14 @@ export const createBodies = (bounds: ICanvasBounds): Matter.Body[] => {
 				fillColor: brightPinkCrayolaColor,
 			}),
 			createRectangle({
-				x: 0,
+				x,
 				y: bounds.height - 4 * HEIGHT_FLOOR,
 				width: 0.25 * bounds.width,
 				height: HEIGHT_FLOOR,
 				fillColor: quinacridoneMagentaColor,
 			}),
 			createTrapezoid({
-				x: 0,
+				x,
 				y: bounds.height - 4.9 * HEIGHT_FLOOR,
 				width: 0.5 * bounds.width,
 				height: 0.25 * bounds.width,
@@ -64,6 +60,26 @@ export const createBodies = (bounds: ICanvasBounds): Matter.Body[] => {
 				fillColor: brightPinkCrayolaColor,
 			}),
 		],
+	});
+};
+
+export const createBodies = (bounds: ICanvasBounds): Matter.Body[] => {
+	const DELTA = 0.25 * bounds.height;
+	const IN_FORMAT_PORTRAIT = bounds.width > 430;
+
+	const sun = createCircle({
+		x: IN_FORMAT_PORTRAIT ? bounds.width * 0.75 : bounds.width,
+		y: DELTA,
+		radio: 0.25 * Math.min(bounds.width, bounds.height),
+		fillColor: brightPinkCrayolaColor,
+	});
+
+	const temple = createTemple({
+		x: IN_FORMAT_PORTRAIT ? bounds.width * 0.5 : 0,
+		bounds: {
+			height: bounds.height,
+			width: IN_FORMAT_PORTRAIT ? bounds.width * 0.5 : bounds.width,
+		},
 	});
 
 	return [sun, temple];
