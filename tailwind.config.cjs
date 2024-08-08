87fd8b44efcbc0199bd3a3e8382873cc2a36140a
Nikolakito <nikolakitoferreiros@gmail.com>
2024-08-08 20:14:24 -0300

    Se inicio el desarrollo del home

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,37 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-// the pallette colors is based in this theme:
-// https://ar.pinterest.com/pin/764908317985213312/
-// https://coolors.co/fd9678-d97c65-f54768-974063-41436a-3e3638
-//
-// /* CSS HEX */
-// --atomic-tangerine: #fd9678ff;
-// --burnt-sienna: #d97c65ff;
-// --bright-pink-crayola: #f54768ff;
-// --quinacridone-magenta: #974063ff;
-// --delft-blue: #41436aff;
-// --jet: #3e3638ff;
-//
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        atomic_tangerine: "#fd9678ff",
-        burnt_sienna: "#d97c65ff",
-        bright_pink_crayola: "#f54768ff",
-        quinacridone_magenta: "#974063ff",
-        delft_blue: "#41436aff",
-        jet: "#3e3638ff",
-      },
-      screens: {
-        tablet: "768px",
-        laptop: "1024px",
-        desktop: "1366px",
-      },
-      boxShadow: {
-        "3xl": "5px 35px 60px -15px rgba(0, 0, 0 )",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				atomic_tangerine: "#fd9678ff",
+				burnt_sienna: "#d97c65ff",
+				bright_pink_crayola: "#f54768ff",
+				quinacridone_magenta: "#974063ff",
+				delft_blue: "#41436A",
+				jet: "#3e3638ff",
+			},
+			screens: {
+				tablet: "768px",
+				laptop: "1024px",
+				desktop: "1366px",
+			},
+			boxShadow: {
+				"3xl": "5px 35px 60px -15px rgba(0, 0, 0 )",
+			},
+			fontFamily: {
+				"roboto-serif": ["Roboto Serif", "serif"], // Agregando la fuente Roboto Serif
+			},
+			fontSize: {
+				96: "6rem",
+				20: "1.5rem",
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
