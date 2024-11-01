9e3b18f24811c26799bef4b0e1c01d202875ea05
Nikolakito <nikolakitoferreiros@gmail.com>
2024-11-01 16:45:54 -0300

    implementacion de pagefind

--- src/store.ts
+++ src/store.ts
@@ -3,4 +3,10 @@ import { atom } from 'nanostores';
 
 export const searchTerm = atom('');
 
+export const setSearchTerm= (term:string)=>{
+    searchTerm.set(term);
+}
 
+ export const getSearchTerm= ()=>{
+    return searchTerm.get()
+}
