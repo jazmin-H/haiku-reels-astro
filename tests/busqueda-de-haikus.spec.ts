10ed10a85c38fe5f1ca80be6df3f951f61c48322
Joaquin Blanco <blancojoaquin77@gmail.com>
2025-01-28 19:40:55 -0300

    refactor:cambio de nombres, nada especial

---
+++ tests/busqueda-de-haikus.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Como lector, deseo un buscador que me permita encontrar haikus por diversos filtros", () => {
+	test("Si uso la plabra clave 'nico', deben aparecer como resultado, los haikus de Nicole y Nicolás", async ({
+		page,
+	}) => {
+		await page.goto("http://localhost:4321/haiku-reels-astro");
+		await page
+			.locator("section")
+			.filter({ hasText: "Haiku Reels La poesía es un" })
+			.getByRole("button")
+			.click();
+		await page.getByRole("link").nth(1).click();
+		await page.getByRole("textbox", { name: "Buscar" }).click();
+		await page.getByRole("textbox", { name: "Buscar" }).fill("nico");
+		await expect(page.getByRole("link", { name: "Por Nicolás Ferreiros" })).toBeVisible();
+		await expect(page.getByRole("link", { name: "Por Nicole Silva" })).toBeVisible();
+	});
+});
